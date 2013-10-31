3ca32d9148e19dfe7c1ff0f4f9c6ce7e1bc743c9
zenoniar@s.dcsdk12.org <apcs@mvhs-u528ap06.dcsdk12.org>
2013-10-31 09:39:06 -0600

    working on shuffle

--- Blackjack/Card.java
+++ Blackjack/Card.java
@@ -4,6 +4,9 @@ public class Card {
 	public Card(int value, String suit) {
 		this.value = value;
 		this.suit = suit;
+		//System.out.println("" + this.value  + " " +this.suit);
 	}
+
+	public void getValue()
 
 }
--- Blackjack/Deck.java
+++ Blackjack/Deck.java
@@ -1,28 +1,56 @@
+import java.util.Random;
 public class Deck {
 	
 	Card[] cards = new Card[52];
+	Random random = new Random();
 
 	public Deck() {
 
 		for (int h = 1; h<= 13; h++) {
-			Card newCard = new Card(h, "Hearts");
+			int i = h;
+			if (h>10) {
+				i = 10;
+			}
+			Card newCard = new Card(i, "Hearts");
 			cards[h-1] = newCard;
 		}
 
 		for (int d = 1; d<= 13; d++) {
-			Card newCard = new Card(d, "Diamonds");
+			int i = d;
+			if (d>10) {
+				i = 10;
+			}
+			Card newCard = new Card(i, "Diamonds");
 			cards[d + 12] = newCard;
 		}
 
 		for (int c = 1; c<= 13; c++) {
-			Card newCard = new Card(d, "Clubs");
+			int i = c;
+			if (c>10) {
+				i = 10;
+			}
+			Card newCard = new Card(i, "Clubs");
 			cards[c + 25] = newCard;
 		}
 
 		for (int s = 1; s<= 13; s++) {
-			Card newCard = new Card(d, "Spades");
+			int i = s;
+			if (s>10) {
+				i = 10;
+			}
+			Card newCard = new Card(i, "Spades");
 			cards[s + 38] = newCard;
 		}
+		shuffle();
 		
+	}
+
+	public void shuffle() {
+		for (int i = cards.length - 1; i > 0; i--) {
+			Card a = cards[i];
+			int temp = random.nextInt(i);
+			cards[i] = cards[temp];
+			cards[temp] = a;
+		}
 	}
 }
